7c0cf544d460baaf01ac1e09294f86a92f3be90b
vineela <vineela19.kanaparthi@gmail.com>
2026-06-04 17:31:51 +0530

    roboshop project started

--- mongodb.sh
+++ mongodb.sh
@@ -41,7 +41,7 @@ VALIDATE $? "Enabling MongoDB"
 systemctl start mongod &>> $LOG_FILE
 VALIDATE $? "Starting MongoDB"
 
-sed -i "s/127.0.0.0/0.0.0.0/g" /etc/mongod.conf
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
 VALIDATE $? "Allowing remote access to MongoDB"
 
 systemctl restart mongod &>> $LOG_FILE
